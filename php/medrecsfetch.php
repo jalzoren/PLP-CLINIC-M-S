8bd2079d55b68bb38d1fc9842de30b0d5717c545
jalzoren <flavierlaurence01@gmail.com>
2025-05-09 16:19:36 +0800

    fetchmedrectable

--- php/medrecsfetch.php
+++ php/medrecsfetch.php
@@ -10,7 +10,7 @@ try {
 
 $sql = "
     SELECT p.Patient_ID, p.First_Name, p.Middle_Name, p.Last_Name, p.Category, p.Sex, 
-           sp.Department AS student_department, pp.Department AS personnel_department
+           sp.Student_ID, pp.Personnel_ID, sp.Department AS student_department, pp.Department AS personnel_department
     FROM patient p
     LEFT JOIN student_patient sp ON p.Patient_ID = sp.Patient_ID
     LEFT JOIN personnel_patient pp ON p.Patient_ID = pp.Patient_ID
@@ -30,12 +30,22 @@ if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $output .= "<tr>
             <td>{$i}</td>
-            <td>{$row['Patient_ID']}</td>
+            <td>";
+
+        if ($row['Student_ID']) {
+            $output .= "{$row['Student_ID']} (Student)";
+        } elseif ($row['Personnel_ID']) {
+            $output .= "{$row['Personnel_ID']} (Personnel)";
+        } else {
+            $output .= "N/A"; 
+        }
+
+        $output .= "</td>
             <td>{$row['First_Name']} {$row['Middle_Name']} {$row['Last_Name']}</td>
             <td>{$row['Category']}</td>
             <td>{$row['Sex']}</td>
             <td>";
-        
+
         if ($row['student_department']) {
             $output .= "Student Department: {$row['student_department']}";
         }
